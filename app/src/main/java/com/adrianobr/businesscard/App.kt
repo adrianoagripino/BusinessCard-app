81a5cdbf87f26b680c28af3539c5f1d258cc41b9
adrianoagripino <adrianobr@adrianobr.com>
2021-08-01 14:47:29 -0300

    Implementing Room Database

--- app/src/main/java/com/adrianobr/businesscard/App.kt
+++ app/src/main/java/com/adrianobr/businesscard/App.kt
@@ -1,6 +1,11 @@
 package com.adrianobr.businesscard
 
 import android.app.Application
+import com.adrianobr.businesscard.data.AppDataBase
+import com.adrianobr.businesscard.data.BusinessCardRepository
 
 class App : Application() {
+    val database by lazy { AppDataBase.getDataBase(this)}
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
+
 }
---
+++ app/src/main/java/com/adrianobr/businesscard/ui/AddBusinessCardActivity.kt
@@ -0,0 +1,27 @@
+package com.adrianobr.businesscard.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adrianobr.businesscard.databinding.ActivityAddBusinessCardBinding
+
+class AddBusinessCardActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityAddBusinessCardBinding.inflate(layoutInflater)}
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        insertListener()
+    }
+
+    private fun insertListener() {
+        binding.btnClose.setOnClickListener {
+            finish()
+        }
+
+        binding.btnConfirm.setOnClickListener {
+
+        }
+    }
+}
